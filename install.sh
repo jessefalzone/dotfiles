6998c1e94bb39e5157f7d92d8c6a8c615847c6a4
Jesse Falzone <jesse.falzone@gmail.com>
2013-08-13 20:12:17 -0400

    Fixed incorrect tmux filename.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ rm ~/.bashrc
 echo "Symlinking files..."
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/tmux ~/.tmux.conf
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/bashrc ~/.bashrc
 
 echo "Done."
