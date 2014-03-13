62e912be49a2f9b501639aa5aba799e80597aa98
Jesse Falzone <jesse.falzone@gmail.com>
2014-03-13 13:21:48 -0400

    Support for new files.

--- install.sh
+++ install.sh
@@ -5,11 +5,15 @@ rm ~/.vimrc
 rm -R ~/.vim
 rm ~/.tmux.conf
 rm ~/.bashrc
+rm ~/.bash_aliases
+rm ~/.bash_profile
 
 echo "Symlinking new files..."
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
 
 echo "Install complete."
